e1362fd68e6406618ca7912738a51346bb97a866
John Gowing <john.gowing@opb.ca>
2024-07-10 18:05:20 -0400

    update build script

--- build.sh
+++ build.sh
@@ -1,8 +1,16 @@
+#!/bin/bash
 export APPS_JSON_BASE64=$(base64 -w 0 ./apps.json)
+VER=$(wget -q -O - https://api.github.com/repos/frappe/hrms/releases/latest | jq -r .name | cut -c2-)
+REL_TAG=opb/frappe-hrms:$VER
+REPO=docker://docker-push.sandbox.opb
 
+echo "building $REL_TAG"
 podman build \
   --build-arg=FRAPPE_PATH=https://github.com/frappe/frappe \
   --build-arg=FRAPPE_BRANCH=version-15 \
   --build-arg=APPS_JSON_BASE64=$APPS_JSON_BASE64 \
-  --tag=opb/frappe-hrms:15.21.1 \
+  --tag=$REL_TAG \
   --file=images/custom/Containerfile .
+
+echo "pushing image to $REPO"
+podman push $REL_TAG $REPO/$REL_TAG
